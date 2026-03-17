9bb67b0323c0930f77ea74072df3ab010fcf0893
Samir Navas <samirnavas7662@gmail.com>
2026-03-17 16:49:34 +0530

    3d

---
+++ 3d/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Scene from "@/components/Scene";
+import Projects from "@/components/Projects";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen">
+      <Scene />
+      <Projects />
+    </main>
+  );
+}
